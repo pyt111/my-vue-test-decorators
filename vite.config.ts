9a448cc7710cc027f7618f14429c996c90f4b5f2
pyt111 <584702675@qq.com>
2024-06-19 14:35:33 +0800

    测试

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import TurboConsole from 'unplugin-turbo-console/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // TurboConsole({
+    //   specifiedEditor: 'webstorm',
+    // })
+  ],
+})
